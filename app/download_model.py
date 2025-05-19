7825ceaa18cfac04fcf6e36765eebc9b41ec5ef2
MariemJammeli <134785464+MariemJammeli@users.noreply.github.com>
2025-05-19 21:40:17 +0100

    Update download_model.py

--- app/download_model.py
+++ app/download_model.py
@@ -1,11 +1,13 @@
 import gdown
 import os
+import zipfile
 
 # Liste des fichiers à télécharger (id, nom de sortie)
 files = [
     ("1cjIbw4Ng44VMqLFYchVTcLA1ZHWbCOwI", "modele_chatbot.zip"),
     ("159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ", "dataset_chatbot.zip"),
 ]
+
 
 
 for file_id, output in files:
@@ -15,3 +17,10 @@ for file_id, output in files:
     url = f"https://drive.google.com/uc?id={file_id}"
     print(f"Téléchargement de {output} ...")
     gdown.download(url, output, quiet=False)
+
+    if output.endswith(".zip"):
+        print(f"Décompression de {output} ...")
+        with zipfile.ZipFile(output, 'r') as zip_ref:
+            zip_ref.extractall(".")
+        print(f"{output} décompressé.")
+
