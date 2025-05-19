b2bb6bf26431a2df73c4a352a2641135e6777648
MariemJammeli <134785464+MariemJammeli@users.noreply.github.com>
2025-05-19 22:14:53 +0100

    Update download_model.py

--- app/download_model.py
+++ app/download_model.py
@@ -2,25 +2,22 @@ import gdown
 import os
 import zipfile
 
-# Liste des fichiers à télécharger (id, nom de sortie)
+# Liste des fichiers à télécharger (URL de partage Google Drive, nom de sortie)
 files = [
-    ("1cjIbw4Ng44VMqLFYchVTcLA1ZHWbCOwI", "modele_chatbot.zip"),
-    ("159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ", "dataset_chatbot.zip"),
+    ("https://drive.google.com/file/d/1cjIbw4Ng44VMqLFYchVTcLA1ZHWbCOwI/view?usp=sharing", "modele_chatbot.zip"),
+    ("https://drive.google.com/file/d/159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ/view?usp=sharing", "dataset_chatbot.zip"),
 ]
 
-
-
-for file_id, output in files:
+for url, output in files:
     if os.path.exists(output):
         print(f"{output} existe déjà. Passage au fichier suivant.")
         continue
-    url = f"https://drive.google.com/uc?id={file_id}"
+
     print(f"Téléchargement de {output} ...")
-    gdown.download(url, output, quiet=False, fuzzy=True)
+    gdown.download(url=url, output=output, quiet=False, fuzzy=True)
 
     if output.endswith(".zip"):
         print(f"Décompression de {output} ...")
         with zipfile.ZipFile(output, 'r') as zip_ref:
             zip_ref.extractall(".")
         print(f"{output} décompressé.")
-
