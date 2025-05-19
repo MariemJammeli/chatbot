81fce36de89c7b01a5e5b1f85854a4ec73e5d212
MariemJammeli <134785464+MariemJammeli@users.noreply.github.com>
2025-05-19 22:57:57 +0100

    Update download_model.py

--- app/download_model.py
+++ app/download_model.py
@@ -2,7 +2,7 @@ import zipfile
 import os
 
 zip_path = "dataset.zip"
-extracted_file = "dataset_chatbot.csv"  # or whatever the actual CSV file is called
+extracted_file = "ml.csv"  # or whatever the actual CSV file is called
 
 if not os.path.exists(extracted_file):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
