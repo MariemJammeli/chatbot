e8a13a6057df39e60c56cca474e7fa2ad72161f0
MariemJammeli <134785464+MariemJammeli@users.noreply.github.com>
2025-05-19 22:36:55 +0100

    Update download_model.py

--- app/download_model.py
+++ app/download_model.py
@@ -2,19 +2,19 @@ import gdown
 import os
 import zipfile
 
-# Liste des fichiers à télécharger (URL de partage Google Drive, nom de sortie)
+# Liste des fichiers à télécharger (ID Google Drive, nom de sortie)
 files = [
-    ("https://drive.google.com/file/d/1cjIbw4Ng44VMqLFYchVTcLA1ZHWbCOwI/view?usp=sharing", "modele_chatbot.zip"),
-    ("https://drive.google.com/file/d/159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ/view?usp=sharing", "dataset_chatbot.zip"),
+    ("159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ", "dataset_chatbot.zip")
 ]
 
-for url, output in files:
+for file_id, output in files:
     if os.path.exists(output):
         print(f"{output} existe déjà. Passage au fichier suivant.")
         continue
 
     print(f"Téléchargement de {output} ...")
-    gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url=url, output=output, quiet=False)
 
     if output.endswith(".zip"):
         print(f"Décompression de {output} ...")
