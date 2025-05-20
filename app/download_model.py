cb8786cc3db0deed550275c4bfb0a0c33afbf4e6
MariemJammeli <134785464+MariemJammeli@users.noreply.github.com>
2025-05-20 09:36:57 +0100

    Update download_model.py

--- app/download_model.py
+++ app/download_model.py
@@ -1,6 +1,16 @@
 import zipfile
 import os
-
+# Liste des fichiers à télécharger (id, nom de sortie)
+files = [
+    ("159Y8TcaaikeDhwp6GTK9IHYRHfICI1QJ", "dataset_chatbot.zip")
+]
+for file_id, output in files:
+    if os.path.exists(output):
+        print(f"{output} existe déjà. Passage au fichier suivant.")
+        continue
+    url = f"https://drive.google.com/uc?id={file_id}"
+    print(f"Téléchargement de {output} ...")
+    gdown.download(url, output, quiet=False)
 zip_path = "app/dataset.zip"
 extracted_file = "ml.csv"  # or whatever the actual CSV file is called
 
